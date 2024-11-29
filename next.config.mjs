812b1b7c12a2fbb9771dd78847fdb811f76ae11b
Thanh Hieu <hieuht.dtu@gmail.com>
2024-11-29 08:54:56 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "loosefstvl.s3.eu-west-2.amazonaws.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
